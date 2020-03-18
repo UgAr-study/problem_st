48a1e5f08fe7e518598f52364011be424f311879
UgAr-study <55754822+UgAr-study@users.noreply.github.com>
2020-03-18 16:34:54 +0300

    work, but without free memory

--- problem_st.c
+++ problem_st.c
@@ -6,6 +6,8 @@
 // mult ::= term {*, /} mult | term
 // term ::= ( expr ) | number
 
+struct node_t* expr (struct lex_array_t lexarr, int* i, struct node_t** ptr_top);
+
 struct node_t *create (struct lexem_t lex) {
     struct node_t* res;
     res = (struct node_t*) calloc (1, sizeof (struct node_t));
@@ -13,44 +15,70 @@ struct node_t *create (struct lexem_t lex) {
     return res;
 }
 
-/*struct node_t* term (struct lex_array_t lexarr, int i) {
-
-}
-
-void mult (struct lex_array_t lexarr, int i) {
-
-}
-
-void expr (struct lex_array_t lexarr, int i) {
-
-}*/
-
 struct node_t* numb (struct lex_array_t lexarr, int i) {
     struct node_t* num;
     if (lexarr.lexems[i].kind != NUM){
-        printf ("Wrong format\n");
+        printf ("N: Wrong format\n");
         exit(1);
     }
     num = create(lexarr.lexems[i]);
     return num;
 }
 
+struct node_t* term (struct lex_array_t lexarr, int *i, struct node_t **ptr_top) {
+    printf ("T:\n");
+    struct node_t* term = NULL;
+    if (lexarr.lexems[*i].kind == NUM) {
+        printf ("term = num\n");
+        term = numb(lexarr, *i);
+        //*i = *i + 1;
+        return term;
+    }
+    if (lexarr.lexems[*i].kind == BRACE) {
+        printf ("term = brace\n");
+        if (lexarr.lexems[*i].lex.b == LBRAC) {
+            *i = *i + 1;
+            term = expr (lexarr, i, ptr_top);
+            return term;
+        }
+        if (lexarr.lexems[*i].lex.b == RBRAC) {
+            *i = *i + 1;
+            return term;
+        }
+    }
+    return term;
+}
+
 struct node_t* mult (struct lex_array_t lexarr, int *i, struct node_t** ptr_top) {
     assert(ptr_top != NULL);
-
-    struct node_t *multy, *tmp;
+    printf ("M:\n");
+    struct node_t *multy, *tmp, *m_left;
 
     multy = (struct node_t*) calloc (1, sizeof (struct node_t));
-    multy->left = numb (lexarr, *i);
+
+    m_left = term (lexarr, i, &multy->left);
+    printf ("after term i = %d\n", *i);
+
+    if (multy->left == NULL)
+        multy->left = m_left;
+
+    printf ("m->l = %p\n", multy->left);
+    //*i = *i + 1;
+    if (multy->left == NULL)
+        return NULL;
+
+    if (*i == lexarr.size) {
+        printf ("i = lexarr.size\n");
+        return multy->left;
+    }
 
     *i = *i + 1;
-
-    if (*i == lexarr.size)
-        return multy->left;
+    printf ("M: check kind; i = %d\n", *i);
 
     if (lexarr.lexems[*i].kind != OP){
-        printf ("Wrong format\n");
-        exit (4);
+        //printf ("M: Wrong format\n");
+        //exit (4);
+        return multy->left;
     }
 
     if ((lexarr.lexems[*i].lex.op != MUL) && (lexarr.lexems[*i].lex.op != DIV))
@@ -62,7 +90,7 @@ struct node_t* mult (struct lex_array_t lexarr, int *i, struct node_t** ptr_top)
     assert(*i != lexarr.size);
     multy->right = mult(lexarr, i, ptr_top);
 
-    if (multy->right->left != NULL) {
+    if (multy->right->left != NULL && abs (multy->lex.lex.op - multy->right->lex.lex.op) <= 1) {
         tmp = multy->right;
         multy->right = multy->right->left;
         tmp->left = multy;
@@ -74,38 +102,44 @@ struct node_t* mult (struct lex_array_t lexarr, int *i, struct node_t** ptr_top)
     return multy;
 }
 
-struct node_t* expr (struct lex_array_t lexarr, int i, struct node_t** ptr_top) {
+struct node_t* expr (struct lex_array_t lexarr, int *i, struct node_t** ptr_top) {
+    static int ent_nub = 0;
+    if (ent_nub == 0) {
+        printf("lexarr.size = %d\n", lexarr.size);
+    }
+    ent_nub++;
+
     assert (ptr_top != NULL);
 
-    struct node_t *expression, *tmp, *e_left, *e_right;
-    if (i == lexarr.size)
+    printf ("E:\n");
+
+    struct node_t *expression, *tmp, *e_left;
+    if (*i == lexarr.size)
         return NULL;
 
     expression = (struct node_t*) calloc (1, sizeof (struct node_t));
 
-    e_left = mult (lexarr, &i, &expression->left);
+    e_left = mult (lexarr, i, &expression->left);
 
     if (expression->left == NULL && e_left != NULL)
         expression->left = e_left;
 
-    if (i == lexarr.size)
+    if (*i == lexarr.size)
         return expression->left;
 
-    if (lexarr.lexems[i].kind != OP) {
-        printf ("Wrong format\n");
-        exit (2);
+    if (lexarr.lexems[*i].kind != OP) {
+        //printf ("E: Wrong format\n");
+        //exit (2);
+        return expression->left;
     }
 
-    expression->lex = lexarr.lexems[i];
-    ++i;
+    expression->lex = lexarr.lexems[*i];
+    *i = *i + 1;
 
-    assert(i != lexarr.size);
+    assert(*i != lexarr.size);
 
     expression->right = expr (lexarr, i, ptr_top);
-    /*e_right = expr (lexarr, i, &expression->right);
-
-    if (expression->right == NULL && e_right != NULL)
-        expression->right = e_right;*/
+    printf ("e->r = %p\n", expression->right);
 
     if (expression->right->left != NULL && abs(expression->lex.lex.num - expression->right->lex.lex.num) <= 1) {
         tmp = expression->right;
@@ -121,14 +155,15 @@ struct node_t* expr (struct lex_array_t lexarr, int i, struct node_t** ptr_top) 
 
 struct node_t* build_syntax_tree(struct lex_array_t lexarr) {
     struct node_t* tree = NULL, *e;
-    e = expr (lexarr, 0, &tree);
+    int i = 0;
+    e = expr (lexarr, &i, &tree);
     assert (e != NULL);
     if (tree == NULL)
         tree = e;
     return tree;
 }
 
-int calc_result(struct node_t *top) { // 22 - 3 + 5 - 6 ответ 20, а верный 18!
+int calc_result(struct node_t *top) {
     int result, l, r;
     assert (top != NULL);
     //left
@@ -215,7 +250,6 @@ void print_tree (struct node_t* top) {
 
     print_tree (top->left);
     print_tree (top->right);
-    return;
 }
 
 void free_syntax_tree(struct node_t * top) {
